0e09a593cda15e02a49989bedb66d3ea318d0be8
Swati <swati.byajbook@gmail.com>
2023-02-07 16:05:32 +0530

    updates

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,38 +1,20 @@
 const Images = {
-  one: require("../../components/Carousel/Multicarouselimg/mc1.webp").default,
-  two: require("../../components/Carousel/Multicarouselimg/mc2.webp").default,
-  three:
-    require("../../components/Carousel/Multicarouselimg/1635315276_5183499.webp")
-      .default,
-  four: require("../../components/Carousel/Multicarouselimg/mc4.webp").default,
-  five: require("../../components/Carousel/Multicarouselimg/1639732857_7381676.webp")
-    .default,
-  six: require("../../components/Carousel/Multicarouselimg/1639234290_1131904.webp")
-    .default,
-  seven:
-    require("../../components/Carousel/Multicarouselimg/1640682855_6029814.webp")
-      .default,
-  eight:
-    require("../../components/Carousel/Multicarouselimg/1637143064_7572589.webp")
-      .default,
-  nine: require("../../components/Carousel/Multicarouselimg/1623322233_7596422.webp")
-    .default,
-  ten: require("../../components/Carousel/Multicarouselimg/1623497436_8623830.webp")
-    .default,
-  eleven: require("../../components/Carousel/Multicarouselimg/mc8.webp")
-    .default,
-  twelve: require("../../components/Carousel/Multicarouselimg/mc7.webp")
-    .default,
-  thirteen:
-    require("../../components/Carousel/Multicarouselimg/1640705462_2512193.webp")
-      .default,
-  fourteen: require("../../components/Carousel/Multicarouselimg/mc5.webp")
-    .default,
-  fifteen: require("../../components/Carousel/Multicarouselimg/mc6.webp")
-    .default,
-  sixteen:
-    require("../../components/Carousel/Multicarouselimg/1640754966_5535532.webp")
-      .default,
+  one: require("../../components/Carousel/Multicarouselimg/mc1.webp"),
+  two: require("../../components/Carousel/Multicarouselimg/mc2.webp"),
+  three: require("../../components/Carousel/Multicarouselimg/1635315276_5183499.webp"),
+  four: require("../../components/Carousel/Multicarouselimg/mc4.webp"),
+  five: require("../../components/Carousel/Multicarouselimg/1639732857_7381676.webp"),
+  six: require("../../components/Carousel/Multicarouselimg/1639234290_1131904.webp"),
+  seven: require("../../components/Carousel/Multicarouselimg/1640682855_6029814.webp"),
+  eight: require("../../components/Carousel/Multicarouselimg/1637143064_7572589.webp"),
+  nine: require("../../components/Carousel/Multicarouselimg/1623322233_7596422.webp"),
+  ten: require("../../components/Carousel/Multicarouselimg/1623497436_8623830.webp"),
+  eleven: require("../../components/Carousel/Multicarouselimg/mc8.webp"),
+  twelve: require("../../components/Carousel/Multicarouselimg/mc7.webp"),
+  thirteen: require("../../components/Carousel/Multicarouselimg/1640705462_2512193.webp"),
+  fourteen: require("../../components/Carousel/Multicarouselimg/mc5.webp"),
+  fifteen: require("../../components/Carousel/Multicarouselimg/mc6.webp"),
+  sixteen: require("../../components/Carousel/Multicarouselimg/1640754966_5535532.webp"),
 };
 
 export default Images;
